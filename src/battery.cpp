ffa13d65f34c837a18a34d6692951f99d927f9cd
Miguel Borges <up202004481@up.pt>
2023-02-15 10:53:09 +0000

    add battery

---
+++ src/battery.cpp
@@ -0,0 +1,10 @@
+#include "functions.h"
+
+std::string battery() {
+	std::ifstream file("/sys/class/power_supply/BATT/capacity");
+	std::stringstream output;
+	int battery;
+	file >> battery;
+	output << battery << '%';
+	return output.str();
+}
